7b18c735fda736d0010ab9f8382f72e50772b8bb
Charan <139607786+TeamKanyarasi@users.noreply.github.com>
2024-04-14 21:40:04 +0530

    Update main.tf

--- terraform/Root/main.tf
+++ terraform/Root/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"            # means any version equal & above
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region#"us-east-1"
+  profile = "default"
+  access_key = "AKIA47CRXROZ2COZH25C" # Not recommended to hardcode
+  secret_key = "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" # Not recommended to hardcode
+}
+
 #Creation Deatils of VPC
 #new commit
 module "vpc" {
